a3b57ca1f18d098cecca2ddfb4b5d4a878cc4aec
Weyxin Ly <weyxin99@fb.com>
2018-06-29 17:59:40 -0700

    update 3

--- app/src/main/java/me/weyxin99/flicks/MovieDetailsActivity.java
+++ app/src/main/java/me/weyxin99/flicks/MovieDetailsActivity.java
@@ -1,10 +1,14 @@
 package me.weyxin99.flicks;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -27,12 +31,14 @@ public class MovieDetailsActivity extends AppCompatActivity {
 
     //the movie to display
     Movie movie;
-
+    String videoID;
     // the view objects
     @BindView(R.id.tvTitle) TextView tvTitle;
     @BindView(R.id.tvOverview) TextView tvOverview;
     @BindView(R.id.rbVoteAverage) RatingBar rbVoteAverage;
+    @BindView(R.id.background) ImageView background;
     AsyncHttpClient client;
+    public final static String TAG = "MovieDetailsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +59,8 @@ public class MovieDetailsActivity extends AppCompatActivity {
         // vote average is 0..10, convert to 0..5 by dividing by 2
         float voteAverage = (float) movie.getVoteAverage();
         rbVoteAverage.setRating(voteAverage > 0 ? voteAverage / 2.0f : voteAverage);
-
+        getVideo(movie);
+        background.setOnClickListener(new click());
     }
 
     //get video
@@ -69,16 +76,42 @@ public class MovieDetailsActivity extends AppCompatActivity {
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 try {
                     JSONArray result = response.getJSONArray("results");
-
-                } catch (JSONException e) {
-                    logError("Failed parsing configuration", e, true);
+                    if(result != null) {
+                       videoID = result.getJSONObject(0).getString("key");
+                    }
+                }
+                catch (JSONException e) {
+                    logError("Result is null", e, true);
                 }
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                logError("Failed getting configuration", throwable, true);
+                logError("Failed to work", throwable, true);
             }
         });
+    }
+
+    //handle error, log and alert user
+    private void logError(String message, Throwable error, boolean alertUser) {
+        //always log the error
+        Log.e(TAG, message, error);
+        //alert the user to avoid silent errors
+        if(alertUser) {
+            //show a long toast with the error message
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //click shows video
+    public class click implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            if(videoID != null) {
+                Intent intent = new Intent(getBaseContext(), MovieTrailerActivity.class);
+                intent.putExtra("video", Parcels.wrap(videoID));
+                getBaseContext().startActivity(intent);
+            }
+        }
     }
 }
--- app/src/main/java/me/weyxin99/flicks/MovieTrailerActivity.java
+++ app/src/main/java/me/weyxin99/flicks/MovieTrailerActivity.java
@@ -8,6 +8,8 @@ import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
+import org.parceler.Parcels;
+
 public class MovieTrailerActivity extends YouTubeBaseActivity {
 
     @Override
@@ -15,8 +17,8 @@ public class MovieTrailerActivity extends YouTubeBaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_trailer);
 
-        // temporary test video id -- TODO replace with movie trailer video id
-        final String videoId = "tKodtNFpzBA";
+        // trailer ID
+        final String videoId = Parcels.unwrap(getIntent().getParcelableExtra("video"));
 
         // resolve the player view from the layout
         YouTubePlayerView playerView = (YouTubePlayerView) findViewById(R.id.player);
