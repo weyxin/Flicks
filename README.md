5ecc734af76bf4a411fcdd9ee21a4d2d66d0f02a
Weyxin Ly <weyxin99@fb.com>
2018-06-29 17:18:27 -0700

    Update README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Project 2 - *Name of App Here*
+# Project 2 - *Flicks*
 
 **Name of your app** shows the latest movies currently playing in theaters. The app utilizes the Movie Database API to display images and basic information about these movies to the user.
 
@@ -17,9 +17,9 @@ The following **optional** features are implemented:
   * [x] Title, Backdrop Image, Overview (Landscape mode)
 * [x] Display a nice default [placeholder graphic](https://guides.codepath.com/android/Displaying-Images-with-the-Glide-Library#advanced-usage) for each image during loading
 * [x] Allow user to view details of the movie including ratings and popularity within a separate activity
-* [ ] Improved the user interface by experimenting with styling and coloring.
+* [x] Improved the user interface by experimenting with styling and coloring.
 * [x] Apply rounded corners for the poster or background images using [Glide transformations](https://guides.codepath.com/android/Displaying-Images-with-the-Glide-Library#transformations)
-* [ ] Apply the popular [Butterknife annotation library](http://guides.codepath.com/android/Reducing-View-Boilerplate-with-Butterknife) to reduce boilerplate code.
+* [x] Apply the popular [Butterknife annotation library](http://guides.codepath.com/android/Reducing-View-Boilerplate-with-Butterknife) to reduce boilerplate code.
 * [ ] Allow video trailers to be played in full-screen using the YouTubePlayerView from the details screen.
 
 The following **additional** features are implemented:
--- app/src/main/java/me/weyxin99/flicks/MovieDetailsActivity.java
+++ app/src/main/java/me/weyxin99/flicks/MovieDetailsActivity.java
@@ -6,11 +6,22 @@ import android.util.Log;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.parceler.Parcels;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import cz.msebera.android.httpclient.Header;
 import me.weyxin99.flicks.models.Movie;
+
+import static me.weyxin99.flicks.MovieListActivity.API_BASE_URL;
+import static me.weyxin99.flicks.MovieListActivity.API_KEY_PARAM;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
@@ -21,12 +32,13 @@ public class MovieDetailsActivity extends AppCompatActivity {
     @BindView(R.id.tvTitle) TextView tvTitle;
     @BindView(R.id.tvOverview) TextView tvOverview;
     @BindView(R.id.rbVoteAverage) RatingBar rbVoteAverage;
+    AsyncHttpClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
-
+        client = new AsyncHttpClient();
         // resolve the view objects
 
         // unwrap the movie passed in via intent, using its simple name as a key
@@ -42,5 +54,31 @@ public class MovieDetailsActivity extends AppCompatActivity {
         float voteAverage = (float) movie.getVoteAverage();
         rbVoteAverage.setRating(voteAverage > 0 ? voteAverage / 2.0f : voteAverage);
 
+    }
+
+    //get video
+    private void getVideo(Movie movie) {
+        //create the url
+        String url = API_BASE_URL + "/movie/" + movie.getId() + "/videos";
+        //set the request params
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, getString(R.string.api_key));
+        //execute a GET request expecting a JSON object response
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONArray result = response.getJSONArray("results");
+
+                } catch (JSONException e) {
+                    logError("Failed parsing configuration", e, true);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                logError("Failed getting configuration", throwable, true);
+            }
+        });
     }
 }
